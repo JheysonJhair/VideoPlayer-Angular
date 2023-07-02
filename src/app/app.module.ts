75e6d4291a4554a8c74fc28565137921ab52ac0c
JheysonJhair <jhair2003unamba@gmail.com>
2023-07-02 14:44:38 -0500

    Creando componenete de video

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,13 @@ import { VgCoreModule } from '@videogular/ngx-videogular/core';
 import { VgControlsModule } from '@videogular/ngx-videogular/controls';
 import { VgOverlayPlayModule } from '@videogular/ngx-videogular/overlay-play';
 import { VgBufferingModule } from '@videogular/ngx-videogular/buffering';
+import { VdoPlayerComponent } from './vdo-player/vdo-player.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VdoPlayerComponent
   ],
   imports: [
     BrowserModule,
